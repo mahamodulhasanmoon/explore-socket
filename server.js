0e6fb6798788cc6772dfa5e1dec5d965a8a1d954
mahamodulhasanmoon <mahamodulhasan.moon@gmail.com>
2023-05-26 17:10:49 +0600

    basic socket setup done

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.get('/', (_req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000, () => {
