9d0f398a16c58e6bc4deaa7290299016cfd9e021
mahamodulhasanmoon <mahamodulhasan.moon@gmail.com>
2023-05-26 21:59:40 +0600

    send data client to server or server to client

--- server.js
+++ server.js
@@ -1,21 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (_req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+// server connection
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  // send data to server
+
+  // setTimeout(() => {
+  //   socket.send('Data send to server to client');
+  // },10000)
+
+  // containous sending data to server
+
+  setInterval(() => {
+    let date = new Date();
+    let time = date.getTime();
+    socket.emit('myEvent',time);
+  }, 100);
+
+// receive data from form 
+
+socket.on('message',function(msg){
+  console.log(msg)
+} )
+
+
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
-  console.log('listening on :3000');
+  console.log("listening on :3000");
 });
